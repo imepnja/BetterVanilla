90a068a356bc7a63fa03d7445e305f5d1e29c335
Imepnja <imepnja@gmail.com>
2020-04-20 21:34:55 +0200

    Loading & Saving & texturating based on image shape & problems: 1)saved png blank 2)png: wiered color changes jpg: black changes to white

--- converter.py
+++ converter.py
@@ -38,24 +38,38 @@ def TexturateSingle(scale, alternation, targetfile='textures/block/stone.png'):
 
     return img2
 
+
+
+
+
 def Texturate(scale, alternation, images=[]):
     newimages = []
 
     for i in range(len(images)):
         filename = images[i].filename
-        img2 = np.zeros((16*scale,16*scale,3), np.uint8)
         img = images[i].img
-    
-        for y in range(16):
-            for x in range(16):
+        image_x = img.shape[0]
+        image_y = img.shape[1]
+        image_c = img.shape[2]
+        print(img.shape)
+        img2 = np.zeros((image_x*scale,image_y*scale,image_c), np.uint8)
+
+        for y in range(image_y):
+            for x in range(image_x):
                 for y2 in range(scale):
                     for x2 in range(scale):
-                        rnd = np.random.randint(low = -alternation, high = alternation)
-                        for c in range(3):
-                            img2[x*scale+x2, y*scale+y2, c] = img[x,y,c] + rnd
-        img2obj = Image(img2, images[i].filename)
-        newimages.append(img2obj)
-    return newimages
+                        if image_c == 4:
+                            if img[x,y, 3] != 0: 
+                                rnd = np.random.randint(low = -alternation, high = alternation)
+                                for c in range(3):
+                                    img2[x*scale+x2, y*scale+y2, c] = img[x,y,c] + rnd
+                        else:
+                            rnd = np.random.randint(low = -alternation, high = alternation)
+                            for c in range(3):
+                                img2[x*scale+x2, y*scale+y2, c] = img[x,y,c] + rnd
+        img2_obj = Image(img2, images[i].filename, images[i].filepath)
+        newimages.append(img2_obj)
+    return newimages    
 
 
 
@@ -64,18 +78,29 @@ def Texturate(scale, alternation, images=[]):
 #Save Image Files
 ####################################################################################
 
-def Load()
+def Load(root):
+    files = []
+    images = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(root):
+        for file in f:
+            files.append(os.path.join(r, file))
 
+
+
+    for i in range(len(files)):
+        images.append(Image(files[i]))
+        # print(str(i) + files[i])
+    return images
+    
 
 ####################################################################################
 #Save Image Files
 ####################################################################################
 
 def Save(images=[]):
-    print(len(images))
     for i in range(len(images)):
-        print(i)
-        print(images[i].filename)
+        print(str(i) + images[i].filename)
         try:
             os.chdir('new_' + images[i].filepath)
         except:
@@ -85,28 +110,49 @@ def Save(images=[]):
         cv2.imwrite(images[i].filename, images[i].img)
         for x in range(len(images[i].filepath.split('/'))):
             os.chdir('..')
-        print(os.getcwd())
+
 
 
 
 scale = 2
 alt = 10
-images = []
-
-stone = Image('textures/block/stone.png')
-oak_planks = Image('textures/block/oak_planks.png')
-
-images.extend([stone, oak_planks])
+new_images = []
+files = Load('textures')
 
 
-#images = [stone, oak_planks]
+print(files[669].filename)
+print(files[669].img.shape)
+Show(files[669].filename + '_', files[669].img)
+
+print(files[1658].filename)
+print(files[1658].img.shape)
+Show(files[1658].filename + '_', files[1658].img)
+#print(files[1658].img)
+
+
+imgs = []
+imgs.append(files[669])
+imgs.append(files[1658])
+
+
+new_images = Texturate(scale, alt, imgs)
+
+for i in new_images:
+    Show(i.filename, i.img)
+
+Save(new_images)
+
+#for f in files:
+ #   new_images.append(Texturate(scale, alt, f))
+    
+
 
 
 ###Saving
-images = Texturate(scale, alt, images)
-Show(images[0].filename, images[0].img)
-Show(images[1].filename, images[1].img)
-Save(images = [stone, oak_planks])
-
+# images = Texturate(scale, alt, images)
+# Show(images[0].filename, images[0].img)
+# Show(images[1].filename, images[1].img)
+# Save(images = [stone, oak_planks])
+# 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- image_obj.py
+++ image_obj.py
@@ -2,13 +2,14 @@ import numpy as np
 import cv2
 
 class Image():
-    def __init__(self, targetfile, filename = 'unknown_filename'):
+    def __init__(self, targetfile, filename = 'unknown_filename', filepath = ''):
         self.filename = filename
         self.targetfile = targetfile
+        self.filepath = filepath
         
 
         if isinstance(targetfile, str):
-            self.img = cv2.imread(targetfile)
+            self.img = cv2.imread(targetfile, cv2.IMREAD_UNCHANGED)
             self.filename = targetfile.split('/')[-1]
             self.filepath = targetfile.rsplit('/', 1)[0]
         else:
